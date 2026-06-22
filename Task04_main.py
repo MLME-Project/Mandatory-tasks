ca66b2cd7bfef7c310c8059cdbddb5252c399437
Julian Krüger <julian3.krueger@tu-dortmund.de>
2026-06-22 13:02:40 +0200

    Addition of the main skript template for Task 4 (Point 5 on the worksheet)

---
+++ Task04_main.py
@@ -0,0 +1,3 @@
+#Task 4
+#A: Implement in addition to the BO-loop other non adaptive strategies. given examples: Sobol, Latin Hypercube sampling. The alternative methods shall stay with in the lowest budget achvied with the BO-Loop. 
+#B: Report: Discuss the results. Comparison non-addaptive vs BO-loop (adaptive)
