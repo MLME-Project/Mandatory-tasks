b7dc5cd8fb2ca07fb57a0fd1c5ff31f12f9053a4
Nils Guhl <nils.guhl@tu-dortmund.de>
2026-06-29 18:23:15 +0200

    Implement gp fit on initial values

--- Task03/Task03_main.py
+++ Task03/Task03_main.py
@@ -1,1 +1,47 @@
-#Hier ist ein Test für Julian
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF, ConstantKernel, WhiteKernel
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from scipy.optimize import minimize
+from API_Group8 import BioreactorClient
+from data_frame_and_csv_manipulation import appendToCSV, getDataFrameFromCSV, getXyFromDataFrame
+
+
+
+T_BOUNDS = [20, 60]
+pH_BOUNDS = [3, 9.5]
+F_BOUNDS = [0, 2]
+
+# setup client
+client = BioreactorClient()
+client.login()
+
+# get initial data
+scale = 'micro'
+fileName = 'micro_data.csv'
+if False:
+    for T in T_BOUNDS:
+        for pH in pH_BOUNDS:
+            for F1 in F_BOUNDS:
+                for F2 in F_BOUNDS:
+                    for F3 in F_BOUNDS:
+                        print(f"running experiment [{scale}, {T}, {pH}, {F1}, {F2}, {F3}]")
+                        result = client.run(scale, T, pH, F1, F2, F3)
+                        appendToCSV(fileName, scale, T, pH, F1, F2, F3, result)
+
+# assemble data
+X, y = getXyFromDataFrame(getDataFrameFromCSV(fileName))
+
+# set up gp
+rbf = ConstantKernel() * RBF() + WhiteKernel()
+gp = GaussianProcessRegressor(kernel=rbf, n_restarts_optimizer=50)
+pipe = Pipeline([
+    ('scaler', StandardScaler()),
+    ('gp', gp)
+    ])
+
+# initial fit
+pipe.fit(X, y)
