ccfabc02d4379814904dc953c2f8e8b5c8fe4197
Nils Guhl <nils.guhl@tu-dortmund.de>
2026-06-30 11:26:20 +0200

    implemented first bo loop

--- Task03/Task03_main.py
+++ Task03/Task03_main.py
@@ -1,47 +1,118 @@
 import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import numpy as np
+from scipy.stats import norm
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF, ConstantKernel, WhiteKernel
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from scipy.optimize import minimize
 from API_Group8 import BioreactorClient
-from data_frame_and_csv_manipulation import appendToCSV, getDataFrameFromCSV, getXyFromDataFrame
+from data_frame_and_csv_manipulation import appendToCSV, getXyFromCSV
 
 
-
+# setup bounds
 T_BOUNDS = [20, 60]
 pH_BOUNDS = [3, 9.5]
 F_BOUNDS = [0, 2]
+
+# setup csv
+FILENAME = 'micro_data.csv'
 
 # setup client
 client = BioreactorClient()
 client.login()
 
-# get initial data
-scale = 'micro'
-fileName = 'micro_data.csv'
-if False:
-    for T in T_BOUNDS:
-        for pH in pH_BOUNDS:
-            for F1 in F_BOUNDS:
-                for F2 in F_BOUNDS:
-                    for F3 in F_BOUNDS:
+
+
+# functions
+def sampleHypercubeCorners(scale, T_Bounds, pH_Bounds, F1_Bounds, F2_Bounds, F3_Bounds, client, fileName):
+    for T in T_Bounds:
+        for pH in pH_Bounds:
+            for F1 in F1_Bounds:
+                for F2 in F2_Bounds:
+                    for F3 in F3_Bounds:
                         print(f"running experiment [{scale}, {T}, {pH}, {F1}, {F2}, {F3}]")
                         result = client.run(scale, T, pH, F1, F2, F3)
                         appendToCSV(fileName, scale, T, pH, F1, F2, F3, result)
+N_INITIAL_SAMPLES = 2**5
 
-# assemble data
-X, y = getXyFromDataFrame(getDataFrameFromCSV(fileName))
 
-# set up gp
-rbf = ConstantKernel() * RBF() + WhiteKernel()
-gp = GaussianProcessRegressor(kernel=rbf, n_restarts_optimizer=50)
-pipe = Pipeline([
+def setupPipeline():
+    rbf = ConstantKernel() * RBF() + WhiteKernel()
+    gp = GaussianProcessRegressor(kernel=rbf, n_restarts_optimizer=50)
+    pipe = Pipeline([
     ('scaler', StandardScaler()),
     ('gp', gp)
     ])
+    return pipe
 
-# initial fit
-pipe.fit(X, y)
+
+# acquisition function
+def expected_improvement(X_candidates, pipeline, y_best, xi=0.01):
+    mu, sigma = pipeline.predict(X_candidates, return_std=True)
+    Z = (mu - y_best - xi) / (sigma + 1e-9)
+    ei = (mu - y_best - xi) * norm.cdf(Z) + sigma * norm.pdf(Z)
+    ei[sigma < 1e-10] = 0.0
+    return ei
+
+
+def getNextAcquisitionFunctionMaximum(pipeline, fileName, acquisitionFunction):
+    # fit data so far and get y_best
+    X, y = getXyFromCSV(fileName)
+    y_best = np.max(y)
+    pipeline.fit(X, y)
+
+    # sample acquisition function
+    n_test = 10000
+    X_test = np.column_stack([
+        np.random.uniform(*T_BOUNDS, n_test),
+        np.random.uniform(*pH_BOUNDS, n_test),
+        np.random.uniform(*F_BOUNDS, n_test),
+        np.random.uniform(*F_BOUNDS, n_test),
+        np.random.uniform(*F_BOUNDS, n_test),
+    ])
+    acq_test = acquisitionFunction(
+        X_candidates=X_test, 
+        pipeline=pipeline, 
+        y_best=y_best)
+    acq_max = np.max(acq_test)
+    X_max = X_test[np.argmax(acq_test)]
+    return X_max, acq_max
+
+
+
+
+# initial data
+# sampleHypercubeCorners(
+#     scale='micro', 
+#     T_Bounds=T_BOUNDS, 
+#     pH_Bounds=pH_BOUNDS, 
+#     F1_Bounds=F_BOUNDS, 
+#     F2_Bounds=F_BOUNDS, 
+#     F3_Bounds=F_BOUNDS, 
+#     client=client, 
+#     fileName=FILENAME)
+
+# set up gp
+pipe = setupPipeline()
+
+# seed rng
+np.random.seed(67)
+
+# main training loop
+for i in range(50):
+    X_max, acq_max = getNextAcquisitionFunctionMaximum(
+        pipeline=pipe,
+        fileName=FILENAME,
+        acquisitionFunction=expected_improvement)
+    scale = "micro"
+    T, pH, F1, F2, F3 = X_max
+    X_max = np.atleast_2d(X_max)
+    y_mu_max, y_sigma_max = pipe.predict(X_max, return_std=True)
+    print(f"next point: T={T:.1f} pH={pH:.2f} F1={F1:.2f} F2={F2:.2f} F3={F3:.2f} | acq={acq_max:.3e} | GP: mu={y_mu_max[0]:.3f} sigma={y_sigma_max[0]:.3f}")
+    result = client.run(scale, T, pH, F1, F2, F3)
+    Y = {result['Y']}
+    print(f"measurement: Y = {Y}")
+    appendToCSV(FILENAME, scale, T, pH, F1, F2, F3, result)
