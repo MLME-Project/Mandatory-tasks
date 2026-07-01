f441368a8875955364ea27f80831b96382a85602
Nils Guhl <nils.guhl@tu-dortmund.de>
2026-07-01 19:02:54 +0200

    add id to parallel coords to view evolution over iterations

--- parallel_coords_plot.py
+++ parallel_coords_plot.py
@@ -4,17 +4,21 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import plotly.express as px
 from data_frame_and_csv_manipulation import getDataFrameFromCSV
 
-CSV_PATH = 'mlme26_group08_2026-06-29T10-47-17.csv'
-# CSV_PATH = 'Task03/micro_data.csv'
+# CSV_PATH = 'mlme26_group08_2026-07-01T16-59-36.csv'
+CSV_PATH = 'Task03/micro_data_ei(0.01)_04.csv'
 
 df = getDataFrameFromCSV(CSV_PATH)
 
 y_min, y_max = df['Y'].min(), df['Y'].max()
 
 for scale in df['scale'].unique():
+    subset = df[df['scale'] == scale]
+
+    subset = subset.sort_values('Y', ascending=True).reset_index()  # 'id' becomes a column, before reordering
+
     fig = px.parallel_coordinates(
-        df[df['scale'] == scale].sort_values('Y', ascending=True),
-        dimensions=['T', 'pH', 'F1', 'F2', 'F3', 'Y'],
+        subset,
+        dimensions=['id', 'T', 'pH', 'F1', 'F2', 'F3', 'Y'],
         color='Y',
         color_continuous_scale=px.colors.sequential.Turbo,
         title=f'Parallel Coordinates — {scale} scale',
